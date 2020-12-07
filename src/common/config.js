ce45b9f433281cc0cb64af0f9808598d44c54aff
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-12-07 23:52:15 +0530

    Creating a congif to refere to resources

---
+++ src/common/config.js
@@ -0,0 +1,15 @@
+var Config =
+{
+    "login": {
+        "username": "admin",
+        "password": "password"
+    },
+    "auth": {
+        "access-token": "IGQVJYZA1NnMHRFclk0cVhNeWJUR25YSWthTDBpdWFoSHlWeGNBUnVuZAmEtNXpmMlpSSTkzRFVJdlAwc0pXeVM0V3JfVFJHN2xpaF9ZAcVdNaHdIVUtBSnh3WEwyUlBBT2o0YVBhd18xdjZAQbjZAmYllXMEZA1V2NxMWt2V09J"        
+    },
+    "api": {
+        mediaUrl: "https://graph.instagram.com/me/media?fields=id,caption",
+        userUrl: "https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption"
+    }
+};
+export default Config;
