9b6927abafef9d2aa8a9a9e359a9ee89794574ab
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-12-07 21:30:26 +0530

    code clean

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,127 +1,160 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './Header.css';
-import logo from '../../assets/logo.svg';
-import Profile from '../../screens/profile/Profile';
-import Input from '@material-ui/core/Input';
-import FormControl from '@material-ui/core/FormControl';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { MenuList, Divider } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { Redirect } from 'react-router-dom'
-import Avatar from '@material-ui/core/Avatar';
+import React, { Component } from "react";
+import "./Header.css";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import FormControl from "@material-ui/core/FormControl";
+import logo from "../../assets/logo.svg";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Profile from "../../screens/profile/Profile";
+import Menu from "@material-ui/core/Menu";
+import ReactDOM from "react-dom";
+import { Redirect } from "react-router-dom";
+import MenuItem from "@material-ui/core/MenuItem";
+import { MenuList, Divider } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
-
-const styles = (theme => ({
+const styles = (theme) => ({
   menuList: {
     width: 150,
     padding: 0,
     marginLeft: 7,
   },
-}))
-
+});
 
 class Header extends Component {
-
   constructor() {
     super();
     this.state = {
       menuIsOpen: false,
-      loggedIn: sessionStorage.getItem("access_token") == null ? false : true,
+      loggedin: sessionStorage.getItem("access_token") == null ? false : true,
       anchorEl: null,
-    }
+    };
   }
-
-
-  // Modal Handler methods for opening and menu list on defined action events
 
   openMenuHandler = () => {
     this.setState({ menuIsOpen: true });
-  }
+  };
 
   closeMenuHandler = () => {
     this.setState({ menuIsOpen: false });
-  }
-
-
-  // click event to execute logout function
-
+  };
 
   logoutClickHandler = () => {
     sessionStorage.removeItem("access_token");
-    this.setState({ loggedIn: false });
-  }
-
-  //action event on my account tab click
+    this.setState({ loggedin: false });
+  };
 
   accountClickHandler = () => {
-    ReactDOM.render(<Profile baseUrl={this.props.baseUrl} />, document.getElementById('root'));
-
-  }
-
-  //action event on profile icon click
+    ReactDOM.render(
+      <Profile baseUrl={this.props.baseUrl} />,
+      document.getElementById("root")
+    );
+  };
 
   onProfileIconClickHandler = (event) => {
-    this.state.anchorEl ? this.setState({ anchorEl: null }) : this.setState({ anchorEl: event.currentTarget });
+    this.state.anchorEl
+      ? this.setState({ anchorEl: null })
+      : this.setState({ anchorEl: event.currentTarget });
     this.openMenuHandler();
-  }
-
+  };
 
   goToLoginPage = () => {
-    if (this.state.loggedIn === false) {
-      return <Redirect to="/" />
+    if (this.state.loggedin === false) {
+      return <Redirect to="/" />;
     }
-  }
-
+  };
   render() {
     const { classes } = this.props;
     return (
       <div>
-        {this.goToLoginPage()}
         <header className="app-header">
-          <a className="logo" href="/home">Image Viewer</a>
-          {/* Search Box Added  */}
-          {this.props.showSearchBox === "true" ?
-            <div className="searchBox" >
+          <a className="logo" href="/home">
+            Image Viewer
+          </a>
+          {this.props.showSearchBox === "true" ? (
+            <div className="searchBox">
               <img src={logo} className="app-logo" alt="Search Logo" />
               <FormControl className="formControl">
-                <Input className="searchText" type="text" placeholder="Search..." disableUnderline={true}
-                  onChange={this.props.searchChangeHandler} />
+                <Input
+                  className="searchText"
+                  type="text"
+                  placeholder="Search..."
+                  disableUnderline={true}
+                  onChange={this.props.searchChangeHandler}
+                />
               </FormControl>
-            </div> : ""}
-          {/* User Profile Icon Added  */}
-          {this.props.loggedIn === true ?
+            </div>
+          ) : (
+            ""
+          )}
+
+         
             <span>
               <IconButton
                 className="iconBtn"
                 size="medium"
-                onClick={event => this.onProfileIconClickHandler(event)} >
+                onClick={(event) => this.onProfileIconClickHandler(event)}
+              >
                 <Avatar className="avatar">
                   <img
+                    src={require("../../assets/masha.jpeg")}
                     className="profilePic"
-                    src={this.props.profilePic}
-                    alt="logged in user profile pic" />
+                    alt="logged in user profile pic"
+                  ></img>
                 </Avatar>
-                </IconButton>
-              <Menu            //Menulist added to click event on profile pic icon of logged in user
-                className="menubar"
+              </IconButton>
+
+              <Menu
+                className="simple-menu"
+                elevation={0}
+                getContentAnchorEl={null}
                 anchorEl={this.state.anchorEl}
-                open={this.state.menuIsOpen}
-                onClose={this.closeMenuHandler}>
-                <MenuList className={classes.menuList}>
-                  {this.props.showAccount === "true" ?
-                    <div>
-                      <MenuItem onClick={this.accountClickHandler}>My Account</MenuItem><Divider varient="middle" />
-                    </div> : ""}
-                  <MenuItem onClick={this.logoutClickHandler}>Logout</MenuItem>
-                </MenuList>
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "center",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                keepMounted
+                open={Boolean(this.state.anchorEl)}
+                onClose={this.handleClose}
+              >
+                <div className={classes.bg}>
+                  <div>
+                    {" "}
+                    <MenuItem
+                      onClose={this.handleClose}
+                      onClick={this.profilePageHandler}
+                    >
+                      <Link to={"/profile"} loggedin="true">
+                        My Account
+                      </Link>
+                    </MenuItem>
+                    <hr />{" "}
+                  </div>
+
+                  <MenuItem
+                    onClose={this.handleClose}
+                    onClick={this.LogoutHandler}
+                  >
+                    <Link to={"/"} loggedin="false">
+                      Logout
+                    </Link>
+                  </MenuItem>
+                </div>
               </Menu>
-            </span> : ""}
-        </header> <br />
-      </div >
-    )
+            </span>
+       
+        </header>
+        <br />
+      </div>
+    );
   }
 }
 
