b1e15aa00fd618aa476b044fc1a399db21329bc3
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-12-07 13:44:53 +0530

    Added route for home and profile

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import Login from '../screens/login/Login';
+import Home from '../screens/home/Home';
+import Profile from '../screens/profile/Profile';
 
 //Controller class for implementing Routing functionality
 
@@ -16,7 +18,9 @@ class Controller extends Component {
     return (
       <Router>
         <div className="main-container">
-           
+            <Route exact path='/' render={(props) => <Login {...props} baseUrl={this.baseUrl}/>} />
+            <Route exact path='/home' render={(props) => <Home {...props} baseUrl={this.baseUrl} />}/>
+            <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />}/>
         </div>
       </Router>
     )
